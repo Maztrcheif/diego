3d5dc7b645a08a90ab8e90de26a6692e6145ad07
Maztrcheif <diego_garcia03@yahoo.com>
2026-02-09 16:04:15 -0800

    actual actaul commit

---
+++ Store/shop.js
@@ -0,0 +1,15 @@
+const products = [
+    {id: 1, name: "banana", category:"produce", price: 1.99},
+    {id: 2, name: "apple", category:"produce", price: 1.49},
+    {id: 3, name: "concha", category: "bakery", price: .99},
+    {id: 4, name: "donut", category: "bakery", price: .99},
+    {id: 5, name: "beef", category: "meat", price: 5.99},
+    {id: 6, name: "chicken", category: "meat", price: 4.99},
+    {id: 7, name: ""}
+]
+const productgrid = document.getElementById(products)
+const searchbar = document.getElementById(search)
+const categoryselect = document.getElementById(category)
+
+
+console.log(products)
