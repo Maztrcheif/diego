1b73ca446352ca2e6bf0e12eb4831743def78a38
Maztrcheif <diego_garcia03@yahoo.com>
2026-03-17 19:57:27 -0700

    wrote crud in php

--- Store/public/api/CRUD.php
+++ Store/public/api/CRUD.php
@@ -13,15 +13,44 @@ if (isset($_POST['action'])) {
 try {
     switch ($action) {
 
-    case 'create'
-        $name = $_POST['name']
-        $category = $_POST['category']
-        $price = $_POST['price']
-        $stmt = $_POST->prepare(
-            "INSERT INTO products (name, category, price") VALUES (?,?,?)"
+    case 'create';
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $price = $_POST['price'];
+        $stmt = $pdo->prepare(
+            "INSERT INTO products (name, category, price) VALUES (?,?,?)" 
             );
-    break;
-    }
-    }
+            $stmt->execute([$name, $category, $price]);
+            echo json_encode(["success" => true]);
+        break;
+    
+    case 'update';
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $price = $_POST['price'];
+        $stmt = $pdo->prepare(
+            "UPDATE products SET name=?, category=?, price=? WHERE id=?"
+        );
+        $stmt->execute([$name, $category, $price]);
+        echo json_encode(["success" => true]);
+        break;
+    
+    case 'delete';
+    $id - $_POST['id'];
+    $stmt = $pdo->prepare("DELETE FROM products WHERE id=?");
+    $stmt->execute([$id]);
+    echo json_encode(["success" => true]);
+         break;
 
+    case 'read';
+
+    $stmt = $pdo->query(
+        "SELECT id, name, category, price FROM products"
+    );
+    echo json_encode($stmt->fetchAll());
+    break;
+    };
+    };
+  
 
